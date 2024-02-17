6f5b9430c3edb7325e5298e63083b30fff5a0606
Pedro Henrique dos Santos Borges <pedrohenrique-dsb@hotmail.com>
2024-02-17 17:22:43 -0300

    limpando codigo

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def my_view(request):
-    return render(request, 'temp/temp.html')
+    return render(request, 'recipes/home.html')
