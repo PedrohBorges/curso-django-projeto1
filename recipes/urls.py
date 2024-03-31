d6919d7c10f80057bfa945d5f8137a1a3871e3b2
Pedro Henrique dos Santos Borges <pedrohenrique-dsb@hotmail.com>
2024-03-31 16:36:36 -0300

    renomeando arquivos 'projeto' para 'project' e modificando o html quando nao tiver nada

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="recipes-home"),
     path('recipes/category/<int:category_id>/', views.category, name="recipes-category"),
-    path('recipes/<int:id>/', views.recipes, name="recipes-recipe"),
+    path('recipe/<int:id>/', views.recipe, name="recipe-recipe"),
     path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -26,8 +26,9 @@ def category(request, category_id):
     })
     
 
-def recipes(request, id):
-    recipe = Recipe.objects.get(id=id)
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe':recipe,
         'is_detail_page': True,
