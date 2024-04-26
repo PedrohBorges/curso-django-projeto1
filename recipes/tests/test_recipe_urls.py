b6b17225174b2d8c2448cf19d1c8ae435235526f
Pedro Henrique dos Santos Borges <pedrohenrique-dsb@hotmail.com>
2024-04-26 15:46:02 -0300

    teste de URLS e VIEWS e BASE realizados e construido um novo teste para os MODEL's ultilizando a biblioteca "parameterized"

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -5,25 +5,25 @@ from django.urls import reverse
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         home_url = reverse('recipes-home')
-        self.assertEqual(home_url, '/')  
-    
+        self.assertEqual(home_url, '/')
+
     def test_recipe_category_url_is_correct(self):
         category_url = reverse('recipes-category', kwargs={'category_id': 1})
         self.assertEqual(category_url, '/recipes/category/1/')
-    
+
     def test_recipe_detail_id_url_is_correct(self):
-        recipe_url = reverse('recipe-recipe', kwargs={'id': 1})
+        recipe_url = reverse('recipes-recipe', kwargs={'id': 1})
         self.assertEqual(recipe_url, '/recipe/1/')
-        
+
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes-home'))
         self.assertEqual(response.status_code, 200)
-        
+
     def test_recipe_home_view_returns_loads_correct_template(self):
         response = self.client.get(reverse('recipes-home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
-        
+
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes-home'))
-        self.assertIn('<h1> No recipes found </h1>', response.content.decode('utf-8'))
-        
+        self.assertIn('<h1> No recipes found </h1>', response.content.decode(
+            'utf-8'))
--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="recipes-home"),
     path('recipes/category/<int:category_id>/', views.category, name="recipes-category"),
-    path('recipe/<int:id>/', views.recipe, name="recipe-recipe"),
+    path('recipe/<int:id>/', views.recipe, name="recipes-recipe"),
     path('admin/', admin.site.urls),
 ]
