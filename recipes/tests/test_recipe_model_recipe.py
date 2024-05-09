43682984c00fb6d9a42addbc5f1d6d7fdd616089
Pedro Henrique dos Santos Borges <pedrohenrique-dsb@hotmail.com>
2024-05-09 18:43:21 -0300

    refatorando o codigo com os testes do search e melhorando a pesquisa do site

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,7 +1,7 @@
-from parameterized import parameterized
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase, Recipe
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -11,13 +11,13 @@ class RecipeModelTest(RecipeTestBase):
 
     def make_recipe_no_defaults(self):
         recipe = Recipe(
-            category=self.make_category(name="test Category Default"),
-            author=self.make_author(username="newuser"),
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
             title='Recipe Title',
             description='Recipe Description',
-            slug='recipe-slug',
-            preparantion_time=10,
-            preparantion_time_unit='Minutos',
+            slug='recipe-slug-for-no-defaults',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
             servings=5,
             servings_unit='Porções',
             preparation_steps='Recipe Preparation Steps',
@@ -29,9 +29,9 @@ class RecipeModelTest(RecipeTestBase):
     @parameterized.expand([
         ('title', 65),
         ('description', 165),
-        ('preparantion_time_unit', 65),
-        ('servings_unit', 65)
-        ])
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
     def test_recipe_fields_max_length(self, field, max_length):
         setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
@@ -41,23 +41,23 @@ class RecipeModelTest(RecipeTestBase):
         recipe = self.make_recipe_no_defaults()
         self.assertFalse(
             recipe.preparation_steps_is_html,
-            msg="Recipe preparation_steps is not False"
-            )
+            msg='Recipe preparation_steps_is_html is not False',
+        )
 
-    def test_recipe_is_published_is_html_is_false_by_default(self):
+    def test_recipe_is_published_is_false_by_default(self):
         recipe = self.make_recipe_no_defaults()
         self.assertFalse(
             recipe.is_published,
-            msg="Recipe is_published is not False"
-            )
+            msg='Recipe is_published is not False',
+        )
 
     def test_recipe_string_representation(self):
-        needed = 'Testing Representation'
+        needed = 'Recipe Title 1'
         self.recipe.title = needed
         self.recipe.full_clean()
         self.recipe.save()
         self.assertEqual(
             str(self.recipe.title), needed,
-            msg=f"Recipe string representation must be "
-            f"'{needed}' but '{str(self.recipe.title)}' was received."
-            )
+            msg=f'Recipe string representation must be '
+                f'"{needed}" but "{str(self.recipe.title)}" was received.'
+        )
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,15 +1,15 @@
+from . import views
 from django.urls import path
 from django.contrib import admin
-from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('', views.home, name="recipes-home"),
+    path('recipes/search/', views.search, name="search"),
     path('recipes/category/<int:category_id>/', views.category, name="recipes-category"),
     path('recipe/<int:id>/', views.recipe, name="recipes-recipe"),
-    path('recipes/search/', lambda request: ..., name="search"),
     path('admin/', admin.site.urls),
 ]
 
